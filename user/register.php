7d340e4cf0c96d65a26724c0a48d84ec9a9349bd
oCharybd1s <108251057+oCharybd1s@users.noreply.github.com>
2022-12-19 15:43:50 +0700

    login/register terbaru

--- user/register.php
+++ user/register.php
@@ -1,9 +1,57 @@
 <?php
     require 'connection.php';
-    $Username = $_POST["Username"];
-    $Email = $_POST["Email"];
-    $Password = $_POST["Password"];
 
-    $sql = "INSERT INTO user (Username, Email, Password) VALUES ('".$Username."' , '".$Email."' , '".$Password."')";
-    $conn->exec($sql);
 ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Halaman Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Register</h1>
+        <form method="POST" ;>
+            <label>Username : </label>
+            <br>
+            <input type="varchar" name="Username" id="username" placeholder="Username">
+            <br>
+            <label>Email : </label>
+            <br>
+            <input type="varchar" name="Email" id="email" placeholder="Email">
+            <br>
+            <label>Password : </label>
+            <br>
+            <input type="password" name="Password" id="password" placeholder="Password">
+            <br>
+            <a href="login.php">
+                <button type="submit" name="submit" class="btn-input">Register</button>
+            </a>
+            <p> Sudah punya akun?
+                <a href="login.php">Login di sini</a>
+            </p>
+        </form>
+        <?php
+            if(isset($_POST['submit'])){
+                $Username = $_POST['Username'];
+                $email = $_POST['Email'];
+                $password = $_POST['Password'];
+                $encryptedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+                $query = mysqli_query($con, "SELECT email FROM user WHere email='$email'");
+                $count = mysqli_num_rows($query);
+
+                if($count > 0){
+                    echo "Cannot register. Already Existed";
+                }
+                else{
+                    $queryInsert = mysqli_query($con, "INSERT INTO user (Username, email, password) VALUES ('$Username','$email', '$password')");
+                }
+            }
+        ?>
+    </div>
+</body>
+
+</html>
