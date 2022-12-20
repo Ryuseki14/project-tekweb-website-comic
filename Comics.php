8210528c030fd58bc77523246d85f0a0d60c5e9b
Ryuseki14 <jovanjonathan90@gmail.com>
2022-12-20 22:43:11 +0700

    rapiin

--- Comics.php
+++ Comics.php
@@ -6,9 +6,6 @@ $comment_post_ID = 1;
 <html>
 
 <head>
-    <style class="anchorjs"></style>
-    <link href="/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="/docs/5.0/assets/css/docs.css" rel="stylesheet">
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -16,46 +13,18 @@ $comment_post_ID = 1;
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="comic.css">
 
-
-    <style type="text/css">
-        img.wp-smiley,
-        img.emoji {
-            display: inline !important;
-            border: none !important;
-            box-shadow: none !important;
-            height: 1em !important;
-            width: 1em !important;
-            margin: 0 0.07em !important;
-            vertical-align: -0.1em !important;
-            background: none !important;
-            padding: 0 !important;
-        }
-    </style>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.css">
-    </link>
+    
     <link rel='stylesheet' id='google-fonts-css' href='https://fonts.googleapis.com/css?family=Lato%3A400%2C500%2C600%2C700%7CPoppins%3A400%2C500%2C600%2C700&#038;ver=2.6.1' type='text/css' media='all' />
-    <link rel='stylesheet' id='style-css' href='https://asura.gg/wp-content/themes/mangastream/style.css?ver=1.1.4' type='text/css' media='all' />
-    <link rel='stylesheet' id='lightstyle-css' href='https://asura.gg/wp-content/themes/mangastream/assets/css/lightmode.css?ver=1.1.4' type='text/css' media='all' />
-    <link rel='stylesheet' id='fontawesome-css' href='https://asura.gg/wp-content/themes/mangastream/assets/css/font-awesome.min.css?ver=5.13.0' type='text/css' media='all' />
-    <link rel='stylesheet' id='owl-carousel-css' href='https://asura.gg/wp-content/themes/mangastream/assets/css/owl.carousel.css?ver=1.0.0' type='text/css' media='all' />
     <script>
-        var baseurl = "https://asura.gg/";
-        var ajaxurl = "https://asura.gg/wp-admin/admin-ajax.php";
         var max_bookmark = 200;
         var max_history = 10;
         var defaultTheme = "darkmode";
-        var tsLogoLight = "https://www.asurascans.com/wp-content/uploads/2021/03/Group_1.png";
-        var tsLogoDark = "https://www.asurascans.com/wp-content/uploads/2021/03/Group_1.png";
     </script>
     <script type='text/javascript' src='jquery.js' id='jquery-js'></script>
     <script type='text/javascript' src='bookmark.js' id='bookmarks_script-js'></script>
     <script type='text/javascript' src='history.js' id='history_script-js'></script>
     <script type='text/javascript' src='function.js' id='tsfn_scripts-js'></script>
     <script type='text/javascript' src='carousel.js' id='owl-carousel-js'></script>
-    <link rel="https://api.w.org/" href="https://asura.gg/wp-json/" />
-    <link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://asura.gg/xmlrpc.php?rsd" />
-    <link rel="wlwmanifest" type="application/wlwmanifest+xml" href="https://asura.gg/wp-includes/wlwmanifest.xml" />
-    <meta name="generator" content="WordPress 6.1.1" />
 </head>
 
 
@@ -199,9 +168,14 @@ $comment_post_ID = 1;
             }
             ?>
         </ol>
-
+            
 
     </section>
+    <div id="footer-placeholder"></div>
+
+    <a href="#" class="scrollToTop" style="display: inline;">
+        <span class="fas fa-angle-up"></span>
+    </a>
 </body>
 
 </html>
@@ -210,5 +184,7 @@ $comment_post_ID = 1;
 <script>
     $(function() {
         $("#nav-placeholder").load("nav.html");
+        $("#footer-placeholder").load("footer.html");
+
     });
 </script>
